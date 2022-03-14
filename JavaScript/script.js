1f598a990573115f345fd2c174f484372888ea18
Marcos José <74626435+marcosjk109@users.noreply.github.com>
2022-03-13 21:41:08 -0300

    Update
    Slide

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,9 @@
 document.querySelector(".hamburguer").addEventListener("click", ()=>
       document.querySelector(".container").classList.toggle("show-menu")
 );
+
+$(function(){
+      $(".quadro-slide").slick({
+            
+      });
+})
