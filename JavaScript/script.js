eeea670377ca280195a219b7d31854559b5b14a2
Marcos José <74626435+marcosjk109@users.noreply.github.com>
2022-03-14 22:39:36 -0300

    Update

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,10 +3,38 @@ document.querySelector(".hamburguer").addEventListener("click", ()=>
 );
 
 $(function(){
-      $(".quadro-slide").slick({
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            centerMode: true
+
+      function atualizar_informacoes() {
+        $("#quadro-name").text( $(".slick-center").data("name") );
+        var quadroPrice = parseFloat($(".slick-center").data("price")).toLocaleString("pt-BR", {style:"currency", currency: "BRL", minimumFractionDigits: 2});
+        $("#quadro-price").text( quadroPrice );
+      }
+    
+      $(".quadro-slide").on('init', function(){
+        atualizar_informacoes();
       });
-})
+    
+      $(".quadro-slide").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        prevArrow: $("#arrow-prev"),
+        nextArrow: $("#arrow-next"),
+        responsive: [
+          {
+            breakpoint: 640,
+            settings: {
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+    
+      $(".quadro-slide").on('afterChange', function(){
+        atualizar_informacoes();
+      });
+    
+    })
+    
+    
